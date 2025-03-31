34c71bb923ce5ba456ca793bd4dcbb13fd600935
Martin L <mlojko.azure@gmail.com>
2025-03-31 15:51:35 -0400

    seed data context update

--- Api/Models/Products/ProductsDbContext.cs
+++ Api/Models/Products/ProductsDbContext.cs
@@ -12,15 +12,24 @@ namespace Api.Models.Products
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Product>().HasData(
-                new Product() {
-                    Id = 1,
-                    Name = "Product 1",
-                    Description = "Description 1",
-                    Price = 10.00m,
-                    Stock = 100
-                }
-                );
+            var products = new List<Product>
+                {
+                    new Product() { Id = 1, Name = "Product 1", Description = "Description 1", Price = 10.00m, Stock = 100 }
+                };
+
+            for (int i = 2; i <= 31; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id = i,
+                    Name = $"Product {i}",
+                    Description = $"Description {i}",
+                    Price = 10.00m * i,
+                    Stock = 100 * i
+                });
+            }
+
+            modelBuilder.Entity<Product>().HasData(products);
         }
     }
 }
